6fbfd5dca9772c74257b23660f31e804e1984de2
Jean Victor Rocha <jean.victor@bunzlbrasil.protcapad.com.br>
2022-10-26 09:57:48 -0300

    atualização de fonte

--- IoT_DVT.ino
+++ IoT_DVT.ino
@@ -1,3 +1,5 @@
+
+
 /********************************************************
  * Jean Victor Rocha
  * IoT DVT
@@ -6,15 +8,23 @@ String FirmwareVer = {"0.2"};
 
 // Bibliotecas ------------------------------------------
 #include <WiFi.h>
+#include <Wire.h>
 #include <HTTPClient.h>
 #include <HTTPUpdate.h>
 #include <WiFiClientSecure.h>
 #include "data\cert.h"//Certificado Github
 #include <NTPClient.h> //Hora NTP - https://github.com/arduino-libraries/NTPClient
+#include <Adafruit_ADS1X15.h>//Biblioteca ADS - 0x48-L;0x49-H;0x4A-SDA;0x4B-SCL
+#include <SSD1306Wire.h>//Biblioteca OLED - 0x3c
 
 #define LEAP_YEAR(Y)     ( (Y>0) && !(Y%4) && ( (Y%100) || !(Y%400) ) )
 #define URL_fw_Version "https://raw.githubusercontent.com/jeanvictorrocha/IoT_DVT/main/version.txt"
 #define URL_fw_Bin     "https://raw.githubusercontent.com/jeanvictorrocha/IoT_DVT/main/build/esp32.esp32.esp32/IoT_DVT.ino.bin"
+
+Adafruit_ADS1115 ADS1115L;  /* Use this for the 16-bit version */
+Adafruit_ADS1015 ADS1015H;  /* Use this for the 12-bit version */
+
+SSD1306Wire oLed_0x3C(0x3c,SDA, SCL);
 
 //Variaveis publicas ------------------------------------------
 String DataAtual = "";
@@ -30,10 +40,10 @@ unsigned long repeatedCallMillis_2 = 0;
 
 //const char * ssid = "CORP-TLG";
 //const char * password = "y.q<5sQWKj#E";
-//const char * ssid = "GRU_Office";
-//const char * password = "K4ilztqh03@#";
-const char * ssid = "J2-VIVO-4G";
-const char * password = "3346878710";
+const char * ssid = "GRU_Office";
+const char * password = "K4ilztqh03@#";
+//const char * ssid = "J2-VIVO-4G";
+//const char * password = "3346878710";
 
 WiFiUDP udp;//Socket UDP que a lib utiliza para recuperar dados sobre o horário
 
@@ -66,12 +76,43 @@ void IRAM_ATTR isr() {
 //#################### SETUP CORE_1 ##########################
 //############################################################
 void setup() {
+  Serial.begin(115200);
+
   pinMode(LED_PIN, OUTPUT);
   digitalWrite(LED_PIN, LOW);
   pinMode(button_boot.PIN, INPUT);
   attachInterrupt(button_boot.PIN, isr, RISING);
+
+  Serial.println("\nI2C Scanner");
+  Wire.begin();
+  CheckI2c();
+  /*
+  0x48 - ADS1115 - Botões interface
+  0x49 - ADS1015 - Sensor de corrente não invasivo SCT-013-100
+  0x4A - ADS1015
+  0x4B - ADS1015
+  0x3C - OLED Display 0.96" 
+  */
+
+  //Inicializa o display Oled
+  oLed_0x3C.init();
+  oLed_0x3C.flipScreenVertically();
   
-  Serial.begin(115200);
+  oLed_0x3C.clear();
+  oLed_0x3C.setFont(ArialMT_Plain_10);
+  oLed_0x3C.setTextAlignment(TEXT_ALIGN_CENTER);
+  oLed_0x3C.drawString(64, 2,"Inicializando...");
+  oLed_0x3C.display();
+
+  //Inicialização do ADS
+  if (!ADS1115L.begin(0x48)) {
+    Serial.println("Failed to initialize ADS1115L(0x48).");
+    while (1);
+  }
+  if (!ADS1015H.begin(0x49)) {
+    Serial.println("Failed to initialize ADS1015H(0x49).");
+    while (1);
+  }
 
   Serial.print("Versão ativa do firmware: ");
   Serial.println(FirmwareVer);
@@ -84,6 +125,8 @@ void setup() {
   delay(2000);
   Serial.println("Inicialização finalizada");
 
+
+  //#################### CORE_0 ################################
   xTaskCreatePinnedToCore(
                     AtualizaDataHora,  /* função que implementa a tarefa */
                     "AtualizaDataHora",/* nome da tarefa */
@@ -94,22 +137,41 @@ void setup() {
                     taskCoreZero);     /* Núcleo que executará a tarefa */
 
   xTaskCreatePinnedToCore(
-                    buttonPressed,  /* função que implementa a tarefa */
-                    "buttonPressed",/* nome da tarefa */
-                    10000,              /* número de palavras a serem alocadas para uso com a pilha da tarefa */
-                    NULL,              /* parâmetro de entrada para a tarefa (pode ser NULL) */
-                    0,                 /* prioridade da tarefa (0 a N) */
-                    NULL,              /* referência para a tarefa (pode ser NULL) */
-                    taskCoreZero);     /* Núcleo que executará a tarefa */
+                    buttonPressed,
+                    "buttonPressed",
+                    10000,
+                    NULL,
+                    1,
+                    NULL,
+                    taskCoreZero);
 
   xTaskCreatePinnedToCore(
-                    repeatedCall,  /* função que implementa a tarefa */
-                    "repeatedCall",/* nome da tarefa */
-                    10000,              /* número de palavras a serem alocadas para uso com a pilha da tarefa */
-                    NULL,              /* parâmetro de entrada para a tarefa (pode ser NULL) */
-                    0,                 /* prioridade da tarefa (0 a N) */
-                    NULL,              /* referência para a tarefa (pode ser NULL) */
-                    taskCoreZero);     /* Núcleo que executará a tarefa */
+                    repeatedCall,
+                    "repeatedCall",
+                    10000,
+                    NULL,
+                    1,
+                    NULL,
+                    taskCoreZero);
+
+  //#################### CORE_1 ################################
+  xTaskCreatePinnedToCore(
+                    ButtonMenu,
+                    "ButtonMenu",
+                    10000,
+                    NULL,
+                    1,
+                    NULL,
+                    taskCoreOne);
+
+  xTaskCreatePinnedToCore(
+                    UpdateDisplay,
+                    "UpdateDisplay",
+                    10000,
+                    NULL,
+                    1,
+                    NULL,
+                    taskCoreOne);
 }
 
 //Função responsavel pela conexão ao WIFI
@@ -129,7 +191,6 @@ void connect_wifi() {
 void setupNTP()
 {
     ntpClient.begin();
-    
     Serial.println("Aguardando primeiro update NTP");
     while(!ntpClient.update())
     {
@@ -137,7 +198,6 @@ void setupNTP()
         ntpClient.forceUpdate();
         delay(500);
     }
-
     Serial.println();
 }
 
@@ -252,7 +312,7 @@ void AtualizaDataHora( void * pvParameters ){
   while(true){
     HoraAtual = ntpClient.getFormattedTime();
     DataAtual = getDate();
-    delay(500);
+    delay(1000);
   }
 }
 
@@ -287,20 +347,21 @@ void repeatedCall( void * pvParameters ){
     }
     if ((currentMillis - repeatedCallMillis_2) >= mini_interval) {
       repeatedCallMillis_2 = currentMillis;
-      Serial.print("idle loop...");
+      Serial.print(HoraAtual);
+      Serial.print(" - idle loop... ");
       Serial.print(num++);
-      Serial.print(" Active fw version:");
+      Serial.print(" Active fw version: ");
       Serial.println(FirmwareVer);
       if(WiFi.status() == WL_CONNECTED) 
       {
-        Serial.println("wifi connected");
+        Serial.println("Wifi connected");
       }
       else
       {
         connect_wifi();
       }
     }
-    delay(500);
+    delay(1000);
   }
 }
 
@@ -312,30 +373,63 @@ void loop() {
   delay(2000);
 }
 
+void ButtonMenu( void * pvParameters ) {
+  while(true){
+    int16_t adc0, adc1, adc2, adc3;
+    float volts0, volts1, volts2, volts3;
 
+    adc0 = ADS1115L.readADC_SingleEnded(0);
+    adc1 = ADS1115L.readADC_SingleEnded(1);
+    adc2 = ADS1115L.readADC_SingleEnded(2);
+    adc3 = ADS1115L.readADC_SingleEnded(3);
 
+    volts0 = ADS1115L.computeVolts(adc0);
+    volts1 = ADS1115L.computeVolts(adc1);
+    volts2 = ADS1115L.computeVolts(adc2);
+    volts3 = ADS1115L.computeVolts(adc3);
 
+    Serial.println("-----------------------------------------------------------");
+    Serial.print("AIN0: "); Serial.print(adc0); Serial.print("  "); Serial.print(volts0); Serial.println("V");
+    Serial.print("AIN1: "); Serial.print(adc1); Serial.print("  "); Serial.print(volts1); Serial.println("V");
+    Serial.print("AIN2: "); Serial.print(adc2); Serial.print("  "); Serial.print(volts2); Serial.println("V");
+    Serial.println("-----------------------------------------------------------");
 
+    delay(1000);
+  }
+}
 
+void CheckI2c() {
+  byte error, address;
+  int nDevices;
+  Serial.println("Scanning...");
+  nDevices = 0;
+  for(address = 1; address < 127; address++ ) {
+    Wire.beginTransmission(address);
+    error = Wire.endTransmission();
+    if (error == 0) {
+      Serial.print("I2C device found at address 0x");
+      if (address<16) {
+        Serial.print("0");
+      }
+      Serial.println(address,HEX);
+      nDevices++;
+    }
+    else if (error==4) {
+      Serial.print("Unknow error at address 0x");
+      if (address<16) {
+        Serial.print("0");
+      }
+      Serial.println(address,HEX);
+    }    
+  }
+  if (nDevices == 0) {
+    Serial.println("No I2C devices found\n");
+  }
+  else {
+    Serial.println("done\n");
+  }
+}
 
+void UpdateDisplay( void * pvParameters ) {
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
