3de0045e2de5f724ff1aea557b9fe89cbc461b40
Jean Victor Rocha <jean.victor@bunzlbrasil.protcapad.com.br>
2022-11-18 16:31:47 -0300

    Inclusão inicial

--- IoT_AtualizaDataHora.ino
+++
@@ -1,8 +0,0 @@
-//Função responsavel pela atualização dos dados de Data e Hora
-void AtualizaDataHora(void *arg){
-  while(true){
-    HoraAtual = ntpClient.getFormattedTime();
-    DataAtual = getDate();
-    delay(1000);
-  }
-}
--- IoT_NTP.ino
+++ IoT_NTP.ino
@@ -10,3 +10,12 @@ void setupNTP() {
     }
     Serial.println();
 }
+
+//Função responsavel pela atualização dos dados de Data e Hora
+void AtualizaDataHora(void *arg){
+  while(true){
+    HoraAtual = ntpClient.getFormattedTime();
+    DataAtual = getDate();
+    delay(1000);
+  }
+}
